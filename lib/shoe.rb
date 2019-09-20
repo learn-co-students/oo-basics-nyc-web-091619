1fe90d863b7bac4a8314802abfb91e9f1bf578ac
divbeech <dyadav1006@gmail.com>
2019-09-20 09:40:22 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,34 @@
-# Make your shoe class here!
+require 'pry'
+# class Shoe
+#     attr_accessor :color, :size, :material, :condition
+#     attr_reader :brand
+
+#     def initialize(brand, condition = "new")
+#         @brand = brand
+#         @condition = condition
+#     end
+
+#     def cobble
+#         @conditon = "new"
+#         puts "Your shoe is as good as new!"
+#     end
+# end 
+
+
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+  
+    def initialize(brand, condition)
+      @brand = brand
+      @condition = condition
+    end
+  
+    def cobble
+      @condition = "new"
+      puts "Your shoe is as good as new!"
+    end
+  end
+
+  s = Shoe.new('sadfg', 'old')
+binding.pry
